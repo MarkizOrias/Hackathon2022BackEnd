9a9ffae40ed3edfa8ecb9a2e92dd94434a97fb45
= <=>
2022-10-28 09:51:23 +0200

    modified:   brownie-config.yaml 	modified:   contracts/ProofOfPropCreator.sol 	new file:   scripts/deploy_mocks.py 	modified:   scripts/deploy_proof.py 	modified:   scripts/helpful_scripts.py

--- contracts/ProofOfPropCreator.sol
+++ contracts/ProofOfPropCreator.sol
@@ -5,16 +5,28 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./ProofOfProp.sol";
 
 contract ProofOfPropCreator {
-
     mapping(address => address[]) private addressToContract;
     ProofOfProp[] private certificatesStorageArray;
+
+    mapping(address => uint256) public addressToAmountFunded; // GB
+    address[] public propClients; // GB
 
     uint256 public usdEntryFee; // variable storing minimum fee
     AggregatorV3Interface internal ethUsdPriceFeed;
 
     constructor(address _priceFeedAddress) {
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress); // Assignment of price feed variable
-        usdEntryFee = 50 * (10 ** 18); // Assignment of minimum fee value
+        usdEntryFee = 50 * (10**18);
+    }
+
+    // GB: created fund function, moved require from addCertificate
+    function fund() public payable {
+        require(
+            msg.value >= getMinimumFee(),
+            "You need to pay more ETH to create certificate!"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
+        propClients.push(msg.sender);
     }
 
     // Client Needs to pay us in order to use "addCertificate" function.
@@ -25,34 +37,53 @@ contract ProofOfPropCreator {
         address _address,
         string memory _name,
         string memory _additional,
-        string memory _hash) public payable {
-            // ToDo :
-            // To use this function client has to pay >= minimumFee.
-            // Money All Clients pay should be stored on ProofOfPropCreator Contract, so as owners of that Contract can withdraw it.
-            require(msg.value >= getMinimumFee(), "You need to pay more ETH to create certificate!");
+        string memory _hash
+    ) public payable returns (address) {
+        // ToDo :
+        // To use this function client has to pay >= minimumFee.
+        // Money All Clients pay should be stored on ProofOfPropCreator Contract, so as owners of that Contract can withdraw it.
 
-            ProofOfProp certificateStorage = new ProofOfProp(_certificate, _date, _title, _address, _name, _additional, _hash);
-            // Below adding new Certificate(Contract) to array, which contains all certificates ever created by all clients.
-            certificatesStorageArray.push(certificateStorage);
-            // Below is mapping Client address with all Certificates(Contracts) he deployed (tracking all certificates, which given Client is owner of).
-            addressToContract[msg.sender].push(address(certificateStorage));
+        ProofOfProp certificateStorage = new ProofOfProp(
+            _certificate,
+            _date,
+            _title,
+            _address,
+            _name,
+            _additional,
+            _hash
+        );
+        // Below adding new Certificate(Contract) to array, which contains all certificates ever created by all clients.
+        certificatesStorageArray.push(certificateStorage);
+        // Below is mapping Client address with all Certificates(Contracts) he deployed (tracking all certificates, which given Client is owner of).
+        addressToContract[msg.sender].push(address(certificateStorage));
+        return address(certificateStorage); // GB: to read deployed POP
     }
 
     // Below Function Allows Client To Check All Certificate(Contracts) He Owns.
-    function getCertificateYouOwn(address _yourAddress) public view returns (address[] memory) {
+    function getCertificateYouOwn(address _yourAddress)
+        public
+        view
+        returns (address[] memory)
+    {
         return addressToContract[_yourAddress];
     }
 
     // Below Function Defines Minimal Fee To Use addCertificate() function.
     function getMinimumFee() public view returns (uint256) {
         (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData(); // Takes this from AggregatorV3 latestRoundData
-        uint256 adjustedPrice = uint256(price) * 10 ** 10; // adjustedPrice has to be expressed with 18 decimals. From Chainlink pricefeed, we know ETH/USD has 8 decimals, so we need to multiply by 10^10
-        uint256 costToEnter = (usdEntryFee * 10 ** 18) / adjustedPrice; // We cannot return decimals, hence we need to express 50$ with 50 * 10*18 / 2000 (adjusted price of ETH)
+        uint256 adjustedPrice = uint256(price) * 10**10; // adjustedPrice has to be expressed with 18 decimals. From Chainlink pricefeed, we know ETH/USD has 8 decimals, so we need to multiply by 10^10
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice; // We cannot return decimals, hence we need to express 50$ with 50 * 10*18 / 2000 (adjusted price of ETH)
         return costToEnter; // for testing
+    }
+
+    // GB: testing purpose - read balance during development. REMOVE IN PRODUCTION VERSION!!!
+    function showBalance() public view returns (uint256) {
+        uint256 POPbalance = address(this).balance;
+        return POPbalance;
     }
 
     // ToDo : Below function allows us as Owners of this contract to withdraw money gathered on this contract.
     // function withdraw() payable onlyOwner public {
 
-    // } 
+    // }
 }
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -8,7 +8,10 @@ PRICE = 200000000000
 
 
 def get_account():
-    if (network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS):
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
@@ -27,3 +30,36 @@ def deploy_mocks():
             # from web3 import Web3 in head, Web3.toWei(2000, "ether") replacing 2000000000000000000000
         )
     print("Mocks deployed!")
+
+
+# contract_to_mock = {"eth_usd_price_feed": MockV3Aggregator}
+
+
+# def get_contract(contract_name):
+#     """This function will grab the contract addresses from the brownie config
+#     if defined, otherwise, it will deploy a mock version of that contract, and
+#     return that mock contract.
+
+#         Args:
+#             contract_name (string)
+
+#         Returns:
+#             brownie.network.contract.ProjectContract: The most recently deployed
+#             version of this contract.
+#     """
+#     contract_type = contract_to_mock[contract_name]
+#     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+#         if len(contract_type) <= 0:
+#             # MockV3Aggregator.length
+#             deploy_mocks()
+#         contract = contract_type[-1]
+#         # MockV3Aggregator[-1]
+#     else:
+#         contract_address = config["networks"][network.show_active()][contract_name]
+#         # address
+#         # ABI
+#         contract = Contract.from_abi(
+#             contract_type._name, contract_address, contract_type.abi
+#         )
+#         # MockV3Aggregator.abi
+#     return contract
